3215fcc292c69b37bcc568429c8ed21f6cfb4a1a
DAVIDE CARIA s309305 <davidejan23@gmail.com>
2024-08-12 22:46:40 +0200

    adding vm infrastructure
    adding vm, disk, nsg, vnet, subnet

--- infrastructure/main.sh
+++ infrastructure/main.sh
@@ -5,15 +5,27 @@ RESOURCE_GROUP="passwordless-transition-poc-deployment"
 LOCATION="EastUS"
 TEMPLATE_DIR="./arm-templates"
 
-# Deploy resource group from template
+###############################
+#### Deploy resource group ####
+###############################
+
 az deployment sub create --location $LOCATION --template-file $TEMPLATE_DIR/resource-group/template.json --parameters @$TEMPLATE_DIR/resource-group/parameters.json
 
-# Deploy SQL infrastructure
+##########################
+#### Deploy SQL infra ####
+##########################
 
-## Pre process the file to generate parameters-final.json with correct secrets
+# Pre process the file to generate parameters-final.json with correct secrets, create a personal config.json file
 chmod +x $TEMPLATE_DIR/sql-infra/parser.sh
 cd $TEMPLATE_DIR/sql-infra || exit
 $TEMPLATE_DIR/sql-infra/parser.sh
 
 cd ../..
 az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEMPLATE_DIR/sql-infra/template.json --parameters @$TEMPLATE_DIR/sql-infra/parameters-final.json
+
+###################
+#### Deploy VM ####
+###################
+
+# modify adminPassword in parameters.json
+az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEMPLATE_DIR/vm-and-network/template.json --parameters @$TEMPLATE_DIR/vm-and-network/parameters.json
