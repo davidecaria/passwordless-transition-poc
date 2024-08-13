6336a70219de9a6f6371709c0aa4c61548e2c1f8
DAVIDE CARIA s309305 <davidejan23@gmail.com>
2024-08-13 12:02:37 +0200

    logic app infrastructure deployment

--- infrastructure/main-infra.sh
+++ infrastructure/main-infra.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+####################################################
+#### Deploy complete infrastructure for the PoC ####
+####################################################
+
+# Main file for the deployment of the infrastructure. Step by step deployment with the help Azure CLI and ARM Templates
+
 # Variables
 RESOURCE_GROUP="passwordless-transition-poc-deployment"
 LOCATION="EastUS"
@@ -29,3 +35,19 @@ az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEM
 
 # modify adminPassword in parameters.json
 az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEMPLATE_DIR/vm-and-network/template.json --parameters @$TEMPLATE_DIR/vm-and-network/parameters.json
+
+###########################
+#### Deploy Logic Apps ####
+###########################
+
+# First logic app - fromUncommittedToStaged
+# Insert correct subscription <@sub_id>
+az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEMPLATE_DIR/logic-apps/la-01/template.json --parameters @$TEMPLATE_DIR/logic-apps/la-01/parameters.json
+
+# Second logic app - TAPissuer
+# Insert correct subscription <@sub_id>
+az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEMPLATE_DIR/logic-apps/la-02/template.json --parameters @$TEMPLATE_DIR/logic-apps/la-02/parameters.json
+
+# Third logic app - fromStagedToCommitted
+# Insert correct subscription <@sub_id>
+az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEMPLATE_DIR/logic-apps/la-03/template.json --parameters @$TEMPLATE_DIR/logic-apps/la-03/parameters.json
